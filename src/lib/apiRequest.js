a34d9dba4e8095b765dd6ea3ed1c9bfd9cf7e16d
Faizan Ahmed <faizan.ahmed@techwards.co>
2024-04-30 15:14:09 +0500

    ip

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("api req called");
 
 const apiRequest = axios.create({
-  baseURL: "http://172.31.42.102:8800/api",
+  baseURL: "http://16.171.129.205:8800/api",
   withCredentials: true,
 });
 
